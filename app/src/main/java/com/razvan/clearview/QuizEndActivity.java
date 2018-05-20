94fb26468e6b3dc4f0a2253b67948955c68747d8
cristianchiru <cristian.r.chiru@gmail.com>
2018-05-20 16:42:37 +0300

    Clear View  - Patient Monitoring System Version 1.0.2 APK Generated - 6.0 is working correctly Final Version - No

--- app/src/main/java/com/razvan/clearview/QuizEndActivity.java
+++ app/src/main/java/com/razvan/clearview/QuizEndActivity.java
@@ -2,6 +2,7 @@ package com.razvan.clearview;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -9,6 +10,18 @@ import com.google.firebase.auth.FirebaseAuth;
 public class QuizEndActivity extends AppCompatActivity {
 
     TextView endText;
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus){
+        super.onWindowFocusChanged(hasFocus);
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState){
--- app/src/main/java/com/razvan/clearview/RecoveryActivity.java
+++ app/src/main/java/com/razvan/clearview/RecoveryActivity.java
@@ -20,6 +20,18 @@ public class RecoveryActivity extends AppCompatActivity {
     private FirebaseAuth auth;
 
     @Override
+    public void onWindowFocusChanged(boolean hasFocus){
+        super.onWindowFocusChanged(hasFocus);
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
 
